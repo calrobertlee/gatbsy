2d23fe76e0387d559b9b59a0a166155060e1eea0
Cal Robert <61516270+garlicagreement@users.noreply.github.com>
2020-07-27 01:44:43 -0700

    filed deletes

---
+++ hello-world/src/pages/bookshelf.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BookPage = () => {
+    return (
+        <div>
+            <h1>Bookshelf</h1>
+            <p>Posts will show up here later on.</p>
+        </div>
+    )
+}
+
+export default BookPage
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -3,13 +3,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Header from "../components/header"
 
-export default function Home() {
-  return <Layout>
-        <div style={{ color: `purple` }}>
+export default function HomePage() {
+  return (
+  <Layout>
+      <div style={{ color: `purple` }}>
         <Header headerText="Hellllo" />
-        <Link to="/about-css-modules">About CSS Modules</Link>
         <p>What a world.</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
       </div>
   </Layout>
+  )
 }
