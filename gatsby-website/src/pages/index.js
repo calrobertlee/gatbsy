d851a1b7dd3c9bf5a35c785672e69192c95a8349
Cal Robert <61516270+garlicagreement@users.noreply.github.com>
2020-07-27 18:10:46 -0700

    gatsby-tailwind-emotion-starter

--- gatsby-website/src/pages/index.js
+++ gatsby-website/src/pages/index.js
@@ -11,16 +11,12 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="hidden" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+		<Image />
     </div>
+	<img className="rounded-md shadow-md" src="https://source.unsplash.com/random/400x200" alt="" />
+
     <Link to="/page-2/">Go to page 2</Link> <br />
-
-    <div class="bg-orange-lightest border-l-4 border-orange text-orange-dark p-4" role="alert">
-      <p class="font-bold">Be Warned</p>
-      <p>Something not ideal might be happening.</p>
-    </div>
-
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
