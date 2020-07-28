8cc39580fdc7a55166ee3141c96b4b61b88250e6
Cal Robert <61516270+garlicagreement@users.noreply.github.com>
2020-07-28 00:38:41 -0700

    twin and styled components

--- v2/src/pages/page-3.js
+++ v2/src/pages/page-3.js
@@ -1,71 +1,26 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
-const Container = styled.div`
-  margin: 3rem auto;
-  max-width: 600px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+const Heading = styled.h1`
+    ${tw`text-2xl text-gray-500 uppercase mt-5`}
+`
+const Img = styled.img`
+    ${tw`mt-10 rounded-md shadow-xl`}
 `
 
-const UserWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 0 auto 12px auto;
-  &:last-child {
-    margin-bottom: 0;
-  }
-`
+//==========
 
-const Avatar = styled.img`
-  flex: 0 0 96px;
-  width: 96px;
-  height: 96px;
-  margin: 0;
-`
-
-const Description = styled.div`
-  flex: 1;
-  margin-left: 18px;
-  padding: 12px;
-`
-
-const Username = styled.h2`
-  margin: 0 0 12px 0;
-  padding: 0;
-`
-
-const Excerpt = styled.p`
-  margin: 0;
-`
-
-const User = props => (
-  <UserWrapper>
-    <Avatar src={props.avatar} alt="" />
-    <Description>
-      <Username>{props.username}</Username>
-      <Excerpt>{props.excerpt}</Excerpt>
-    </Description>
-  </UserWrapper>
+const ThirdPage = () => (
+    <Layout>
+      <Heading>Hi from the second page</Heading>
+      <Img src="https://source.unsplash.com/random/400x200" alt="" />
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
 )
-
-export default function UsersList() {
-  return (
-    <Container>
-      <h1>About Styled Components</h1>
-      <p>Styled Components is cool</p>
-      <User
-        username="Jane Doe"
-        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
-        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-      />
-      <User
-        username="Bob Smith"
-        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
-        excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-      />
-    </Container>
-  )
-}
+  
+export default ThirdPage
+  
